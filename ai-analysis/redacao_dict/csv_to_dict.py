93396ec7635bed994b6e27e3bd9a74b4fcdffb00
Guilherme27Souza <guilhermeoliveiradesouza@gmail.com>
2025-09-16 20:51:34 -0300

    analise de features para entrega 17/9

--- ai-analysis/redacao_dict/csv_to_dict.py
+++ ai-analysis/redacao_dict/csv_to_dict.py
@@ -1,6 +1,7 @@
+import numpy as np
 import pandas as pd
 
-def create_redacoes_dict(num_amostras=6557): 
+def create_redacoes_dict(num_amostras=6557):
 
     extended_essay_br = pd.read_csv('../database/extended_essay-br.csv')
     info_redacoes = pd.read_csv('../database/prompts.csv')
@@ -11,33 +12,24 @@ def create_redacoes_dict(num_amostras=6557):
 
     print(f"Selecionando {num_amostras} redações aleatórias do total de {len(extended_essay_br)}...")
     
-    df_amostra = extended_essay_br.sample(n=num_amostras, random_state=42)
+    indices_disponiveis = extended_essay_br.index.to_numpy()
+    indices_amostra = np.random.choice(indices_disponiveis, size=num_amostras, replace=False)
+    df_amostra = extended_essay_br.loc[indices_amostra]
 
     dados_estruturados = []
-
     for index, row in df_amostra.iterrows():
-
         info = info_redacoes.loc[int(row['prompt'])]
-
-        enunciado = ""
-        for linha in info['description']:
-            enunciado+=linha
-
+        enunciado = "".join(info['description'])
         objeto_redacao = {
             'id': index,
             'titulo': row['title'],
             'texto': row['essay'],
             'nota_c1': row['c1'],
-            'nota_c2': row['c2'],
-            'nota_c3': row['c3'],
-            'nota_c4': row['c4'],
-            'nota_c5': row['c5'],
             'nota_final': row['score'],
             'enunciado_titulo': info['title'],
             'enunciado': enunciado,
             'categoria': info['category']
         }
-
         dados_estruturados.append(objeto_redacao)
 
     return dados_estruturados
